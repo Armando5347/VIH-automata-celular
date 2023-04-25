0f0e787d011890d2cb2a55e99afbc60fa37e6acd
SaidHoffman <128929135+SaidHoffman@users.noreply.github.com>
2023-04-25 09:25:56 -0600

    Update automata_celular.cpp

--- automata_celular.cpp
+++ automata_celular.cpp
@@ -53,87 +53,80 @@ public:
         
     }
 
-    void actualizar_celula(celula celula_a_actualizar, int pos_x, int pos_y){
+    //analizar vecindad de Moore
+    void analizar_vecindad(int pos_x, int pos_y, short int &celulas_infectadas_adyacentes_tipo_A, short int &celulas_infectadas_adyacentes_tipo_B) {
+        // Iterar sobre las 8 celdas vecinas
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                // Evitar la celda central (0, 0)
+                if (dx == 0 && dy == 0) continue;
+
+                int vecino_x = (pos_x + dx + _L) % _L;
+                int vecino_y = (pos_y + dy + _L) % _L;
+
+                // No es necesario verificar si esta dentro de la matriz ya que con el modulo funciona como un toroide
+                celula &vecino = matriz_celulas[vecino_x][vecino_y];
+
+                if (vecino.get_estado() == celula::estado_celula_infentada_A) {
+                    celulas_infectadas_adyacentes_tipo_A++;
+                } else if (vecino.get_estado() == celula::estado_celula_infectada_B) {
+                    celulas_infectadas_adyacentes_tipo_B++;
+                }
+            }
+        }
+    }
+
+    void actualizar_celula(celula &celula_a_actualizar, int pos_x, int pos_y){
+        short int celulas_infectadas_adyacentes_tipo_A = 0;
+        short int celulas_infectadas_adyacentes_tipo_B = 0;
+        analizar_vecindad(pos_x, pos_y, celulas_infectadas_adyacentes_tipo_A, celulas_infectadas_adyacentes_tipo_B);
 
         switch (celula_a_actualizar.get_estado())
         {
-        case celula::estado_celula_sana :
+            case celula::estado_celula_sana :
             {
-                //se tiene que revisar la vecindad de Moore
-            
-            //aquí se INSERTA EL ANÁLISIS DE LA VECINDAD
-            void analizar_vecindad(int pos_x, int pos_y) {
-                short int celulas_infectadas_adyacentes_tipo_A = 0;
-                short int celulas_infectadas_adyacentes_tipo_B = 0;
-                    // Iterar sobre las 8 celdas vecinas
-                    for (int dx = -1; dx <= 1; dx++) {
-                        for (int dy = -1; dy <= 1; dy++) {
-                            // Evitar la celda central (0, 0)
-                            if (dx == 0 && dy == 0) continue;
-
-                            int vecino_x = (pos_x + dx + _L) % _L;
-                            int vecino_y = (pos_y + dy + _L) % _L;
-
-                            // No es necesario verificar los límites de la matriz, porque las condiciones de frontera toroidal
-                            // ya se manejan con el cálculo del módulo
-
-                            celula &vecino = matriz_celulas[vecino_x][vecino_y];
-
-                            if (vecino.get_estado() == celula::estado_celula_infentada_A) {
-                                celulas_infectadas_adyacentes_tipo_A++;
-                            } else if (vecino.get_estado() == celula::estado_celula_infectada_B) {
-                                celulas_infectadas_adyacentes_tipo_B++;
-                            }
-                        }
-                    }
-                }
-            
                 // Aplicar las reglas de cambio de estado
-                celula &celula_actual = matriz_celulas[pos_x][pos_y];
-                if (celula_actual.get_estado() == celula::estado_celula_sana) {
-                    if (celulas_infectadas_adyacentes_tipo_A >= _RA || celulas_infectadas_adyacentes_tipo_B >= _RB) {
-                        celula_actual.set_estado(celula::estado_celula_infentada_A);
-                    }
+                if (celulas_infectadas_adyacentes_tipo_A >= _RA || celulas_infectadas_adyacentes_tipo_B >= _RB) {
+                    celula_a_actualizar.set_estado(celula::estado_celula_infentada_A);
                 }
+                break;
             }
+            case celula::estado_celula_infentada_A :
+            {
+                //se revisa si pasaron los 't' periodos o no
+                if(celula_a_actualizar.get_periodo() > 0)
+                    celula_a_actualizar.paso_periodo();
+                else
+                    celula_a_actualizar.set_estado(celula::estado_celula_infectada_B);
+                break;
+            }
+            case celula::estado_celula_infectada_B :
+            {
+                celula_a_actualizar.set_estado(celula::estado_celula_muerta);
+                break;
+            }
+            case celula::estado_celula_muerta:
+            {
+                // Generar un número aleatorio en el rango [0, 1]
+                double random_prob = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
 
-            
-            break;
-        case celula::estado_celula_infentada_A :
-            {
-            //se revisa si pasaron los 't' periodos o no
-            if(celula_a_actualizar.get_periodo() > 0)
-                celula_a_actualizar.paso_periodo();
-            else
-                celula_a_actualizar.set_estado(celula::estado_celula_infectada_B);
+                // Comparar el número aleatorio con las probabilidades
+                if (random_prob <= _pinfec) {
+                    celula_a_actualizar.set_estado(celula::estado_celula_infentada_A); // Reemplazar por una célula infectada tipo A
+                } else if (random_prob <= (_pinfec + _prepl)) {
+                    celula_a_actualizar.set_estado(celula::estado_celula_sana); // Reemplazar por una célula sana
+                }
+                break;
             }
-            break;
-        case celula::estado_celula_infectada_B :
-            {
-            celula_a_actualizar.set_estado(celula::estado_celula_muerta);
-            }
-            break;
-        case celula::estado_celula_muerta:
-            {
-            // Generar un número aleatorio en el rango [0, 1]
-            double random_prob = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
-
-            // Comparar el número aleatorio con las probabilidades
-            if (random_prob <= _pinfec) {
-                celula_a_actualizar.set_estado(celula::estado_celula_infentada_A); // Reemplazar por una célula infectada tipo A
-            } else if (random_prob <= (_pinfec + _prepl)) {
-                celula_a_actualizar.set_estado(celula::estado_celula_sana); // Reemplazar por una célula sana
-            }
-            }
-            break;
-        default:
-            //"Si llegamos aquí, hubo un error en las estados de una célula"<<endl;
-            break;
+            default:
+                //"Si llegamos aquí, hubo un error en las estados de una célula"<<endl;
+                break;
         }
-        
     }
 
     //faltan getters y setters    
 
 };
+
+                
 
