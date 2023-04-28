a00d025522d0ea9dc34bb094fb2dce17a002f1e0
SaidHoffman <128929135+SaidHoffman@users.noreply.github.com>
2023-04-27 23:26:39 -0600

    Update NodoDensidad.java

--- NodoDensidad.java
+++ NodoDensidad.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automatacelular;
+
 public class NodoDensidad {
     public double densidadCelulasSanas;
     public double densidadCelulasInfectadas;
@@ -9,5 +16,32 @@ public class NodoDensidad {
         densidadCelulasInfectadas = 0;
         densidadCelulasMuertas = 0;
         siguienteDensidad = null;
+    }
+
+    public NodoDensidad(double densidadSanas, double densidadInfectadas, double densidadMuertas) {
+        this.densidadCelulasSanas = densidadSanas;
+        this.densidadCelulasInfectadas = densidadInfectadas;
+        this.densidadCelulasMuertas = densidadMuertas;
+        this.siguienteDensidad = null;
+    }
+
+    public double getDensidadCelulasSanas() {
+        return densidadCelulasSanas;
+    }
+
+    public double getDensidadCelulasInfectadas() {
+        return densidadCelulasInfectadas;
+    }
+
+    public double getDensidadCelulasMuertas() {
+        return densidadCelulasMuertas;
+    }
+
+    public void setSiguiente(NodoDensidad siguiente) {
+        this.siguienteDensidad = siguiente;
+    }
+
+    public NodoDensidad getSiguiente() {
+        return siguienteDensidad;
     }
 }
