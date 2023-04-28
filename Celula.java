f8f3271676b35ebd64b8fc65937d51a618afe49c
SaidHoffman <128929135+SaidHoffman@users.noreply.github.com>
2023-04-27 22:33:34 -0600

    Create Celula.java

---
+++ Celula.java
@@ -0,0 +1,44 @@
+public class Celula {
+    public static final int estado_celula_sana = 0;
+    public static final int estado_celula_infentada_A = 1;
+    public static final int estado_celula_infectada_B = 2;
+    public static final int estado_celula_muerta = 3;
+
+    private int estado;
+    private int estadoFuturo;
+    private int periodosRestantes;
+
+    public Celula() {
+        estado = estado_celula_sana;
+        estadoFuturo = estado_celula_sana;
+        periodosRestantes = 0;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getEstadoFuturo() {
+        return estadoFuturo;
+    }
+
+    public void setEstadoFuturo(int estadoFuturo) {
+        this.estadoFuturo = estadoFuturo;
+    }
+
+    public int getPeriodo() {
+        return periodosRestantes;
+    }
+
+    public void setPeriodos(int periodos) {
+        this.periodosRestantes = periodos;
+    }
+
+    public void pasoPeriodo() {
+        this.periodosRestantes--;
+    }
+}
