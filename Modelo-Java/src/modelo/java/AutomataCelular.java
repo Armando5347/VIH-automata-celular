560a7cc8b154dc7fb344772a85a1277f0822ea9e
SaidHoffman <128929135+SaidHoffman@users.noreply.github.com>
2023-04-29 14:48:27 -0600

    Update AutomataCelular.java

--- Modelo-Java/src/modelo/java/AutomataCelular.java
+++ Modelo-Java/src/modelo/java/AutomataCelular.java
@@ -5,29 +5,30 @@
  */
 package modelo.java;
 
-import java.util.Random;
-
+/**
+ * La clase AutomataCelular representa un autómata celular que simula la evolución temporal de un tejido celular
+ * en el que puede haber células sanas, infectadas tipo A, infectadas tipo B y muertas.
+ */
 public class AutomataCelular {
-    private int L;
-    private int RA;
-    private int RB;
-    private Celula[][] matrizCelulas;
-    private int t;
-    private double prepl;
-    private double pinfec;
-    private double pVIH;
-    private NodoDensidad cabeceraDensidades;
-    private NodoDensidad densidadActual;
-    private NodoDensidad nodoDensidadAuxiliar;
-    private double p_i;//inecesarias, checar eso
-    private double p_m;
-    private Random random;
-    private int celulasInfectadasA;
-    private int celulasInfectadasB;
+    // Se definen los atributos de la clase
+    private int L; // Tamaño de la matriz cuadrada de células
+    private int RA; // Radio de infectividad de células tipo A
+    private int RB; // Radio de infectividad de células tipo B
+    private int t; // Periodo de incubación de células tipo A
+    private double pinfec; // Probabilidad de infección por células infectadas
+    private double prepl; // Probabilidad de reparación de células muertas
+    private double pVIH; // Porcentaje de células infectadas con VIH
+    private Celula[][] matrizCelulas; // Matriz de células
+    private NodoDensidad cabeceraDensidades; // Nodo inicial de la lista enlazada de densidades
+    private NodoDensidad densidadActual; // Nodo actual de la lista enlazada de densidades
+    private Random random; // Generador de números aleatorios
 
+    // Se define el constructor vacío
     public AutomataCelular() {}
 
-    public AutomataCelular(int L, int ra, int rb, int t, double p_infec, double p_vih, double p_i, double p_m) {
+    // Se define el constructor con parámetros
+    public AutomataCelular(int L, int ra, int rb, int t, double p_infec, double p_vih) {
+        // Se inicializan los atributos con los valores recibidos
         this.L = L;
         this.RA = ra;
         this.RB = rb;
@@ -35,11 +36,10 @@ public class AutomataCelular {
         this.pinfec = p_infec;
         this.prepl = 1 - p_infec;
         this.pVIH = p_vih;
-        this.p_i = p_i;
-        this.p_m = p_m;
         this.random = new Random();
         this.matrizCelulas = new Celula[L][L];
 
+        // Se inicializa la matriz de células
         for (int i = 0; i < L; i++) {
             for (int j = 0; j < L; j++) {
                 this.matrizCelulas[i][j] = new Celula();
@@ -48,118 +48,137 @@ public class AutomataCelular {
             }
         }
 
-        int totalCelulasInfectadas = (int) (L * L * p_vih);
-        int celulasInfectadas = 0;
-
-        while (celulasInfectadas <= totalCelulasInfectadas) {
+        // Se infectan células aleatorias con VIH
+        int total_celulas_infectadas_iniciales = (int) ((L * L * p_vih) / 1); 
+        this.cabeceraDensidades = new NodoDensidad();
+        this.cabeceraDensidades.densidadCelulasSanas = (L*L - total_celulas_infectadas_iniciales) / (L*L);
+        this.cabeceraDensidades.densidadCelulasInfectadas = total_celulas_infectadas_iniciales / (L*L);
+        this.cabeceraDensidades.densidadCelulasMuertas = 0;
+        this.densidadActual = this.cabeceraDensidades;
+        while (total_celulas_infectadas_iniciales > 0){
             int i = random.nextInt(L);
             int j = random.nextInt(L);
 
-            if (matrizCelulas[i][j].getEstado() == Celula.ESTADO_CELULA_SANA) {
+            if (matrizCelulas[i][j].getEstado() != Celula.ESTADO_CELULA_INFECTADA_A) {
                 matrizCelulas[i][j].setEstado(Celula.ESTADO_CELULA_INFECTADA_A);
-                celulasInfectadas++;
+
+                total_celulas_infectadas_iniciales--;
             }
         }
-
-        this.cabeceraDensidades = new NodoDensidad();
-        this.cabeceraDensidades.densidadCelulasInfectadas = totalCelulasInfectadas / (L*L);
-        this.cabeceraDensidades.densidadCelulasInfectadas = ( L*L - totalCelulasInfectadas ) / ( L*L);
-        this.cabeceraDensidades.densidadCelulasMuertas = 0;
-        this.densidadActual = this.cabeceraDensidades;
     }
-
+    
     public void ejecutarSimulacion(int pasos) {
-        for (int paso = 0; paso < pasos; paso++) {
-            for (int i = 0; i < L; i++) {
-                for (int j = 0; j < L; j++) {
-                    actualizarEstadoFuturo(i, j);
+            for (int i = 0; i < pasos; i++) {
+           actualizar_automata();
+       }
+    }   
+    
+ void actualizar_automata() {
+    int iterador1, iterador2;
+    // primero se calculan los valores futuros
+    for (iterador1 = 0; iterador1 < this.L; iterador1++) {
+        for (iterador2 = 0; iterador2 < this.L; iterador2++) {
+            actualizar_celula(this.matrizCelulas[iterador1][iterador2], iterador1, iterador2);
+        }
+    }
+
+    // se instancia un nuevo nodo de densidad (suponiendo que se mida por semanas)
+    NodoDensidad nodoDensidadAuxiliar = this.densidadActual;
+    this.densidadActual = new NodoDensidad();
+    nodoDensidadAuxiliar.siguienteDensidad = this.densidadActual;
+    this.densidadActual.siguienteDensidad = null;
+    this.densidadActual.densidadCelulasInfectadas = 0;
+    this.densidadActual.densidadCelulasSanas = 0;
+    this.densidadActual.densidadCelulasMuertas = 0;
+
+    // inicializar en CERO las densidades
+    // luego, se actualizan los valores
+    int newEstado;
+    for (iterador1 = 0; iterador1 < this.L; iterador1++) {
+        for (iterador2 = 0; iterador2 < this.L; iterador2++) {
+            newEstado = this.matrizCelulas[iterador1][iterador2].getEstadoFuturo();
+            this.matrizCelulas[iterador1][iterador2].setEstado(newEstado);
+            if (newEstado == Celula.ESTADO_CELULA_SANA) {
+                this.densidadActual.densidadCelulasSanas++;
+            } else if (newEstado == Celula.ESTADO_CELULA_MUERTA) {
+                this.densidadActual.densidadCelulasMuertas++;
+            } else {
+                this.densidadActual.densidadCelulasInfectadas++;
+            }
+        }
+    }
+
+    // dividir entre L * L cada densidad
+    this.densidadActual.densidadCelulasInfectadas /= this.L * this.L;
+    this.densidadActual.densidadCelulasSanas /= this.L * this.L;
+    this.densidadActual.densidadCelulasMuertas /= this.L * this.L;
+}
+
+// Esta función analiza la vecindad de una célula en una posición dada y cuenta el número de células infectadas tipo A y B adyacentes.
+void analizar_vecindad(int pos_x, int pos_y, short[] celulas_infectadas_adyacentes_tipo_A, short[] celulas_infectadas_adyacentes_tipo_B) {
+    for (int dx = -1; dx <= 1; dx++) {
+        for (int dy = -1; dy <= 1; dy++) {
+            if (dx == 0 && dy == 0) continue;
+            int vecino_x = (pos_x + dx + this.L) % this.L;
+            int vecino_y = (pos_y + dy + this.L) % this.L;
+            Celula vecino = this.matrizCelulas[vecino_x][vecino_y];
+
+            if (vecino.getEstado() == Celula.ESTADO_CELULA_INFECTADA_A) {
+                celulas_infectadas_adyacentes_tipo_A[0]++;
+            } else if (vecino.getEstado() == Celula.ESTADO_CELULA_INFECTADA_B) {
+                    celulas_infectadas_adyacentes_tipo_B[0]++;
                 }
             }
+        }
+    }
+    
+void actualizar_celula(Celula celula_a_actualizar, int pos_x, int pos_y) {
+        celula_a_actualizar.setEstadoFuturo(celula_a_actualizar.getEstado());
 
-            for (int i = 0; i < L; i++) {
-                for (int j = 0; j < L; j++) {
-                    matrizCelulas[i][j].setEstado(matrizCelulas[i][j].getEstadoFuturo());
+        short[] celulas_infectadas_adyacentes_tipo_A = {0};
+        short[] celulas_infectadas_adyacentes_tipo_B = {0};
+
+        switch (celula_a_actualizar.getEstado()) {
+            case Celula.ESTADO_CELULA_SANA:
+                analizar_vecindad(pos_x, pos_y, celulas_infectadas_adyacentes_tipo_A, celulas_infectadas_adyacentes_tipo_B);
+                if (celulas_infectadas_adyacentes_tipo_A[0] >= this.RA || celulas_infectadas_adyacentes_tipo_B[0] >= this.RB) {
+                    celula_a_actualizar.setEstadoFuturo(Celula.ESTADO_CELULA_INFECTADA_B);
                 }
+                break;
+
+            case Celula.ESTADO_CELULA_INFECTADA_A:
+                
+            if (celula_a_actualizar.getPeriodo()> 0) {
+                celula_a_actualizar.pasoPeriodo();
+            } else {
+                celula_a_actualizar.setEstadoFuturo(Celula.ESTADO_CELULA_INFECTADA_B);
             }
-            calcularDensidades();
-        }
-    }
+            break;
 
-   private void actualizarEstadoFuturo(int i, int j) {
-    Celula celula = matrizCelulas[i][j];
-    int estado = celula.getEstado();
-    int vecinosInfectados = contarVecinosInfectados(i, j);
+        case Celula.ESTADO_CELULA_INFECTADA_B:
+            celula_a_actualizar.setEstadoFuturo(Celula.ESTADO_CELULA_MUERTA);
+            break;
 
-    if (estado == Celula.ESTADO_CELULA_SANA && vecinosInfectados > 0) {
-        double probabilidadInfeccion = 1 - Math.pow((1 - p_i), vecinosInfectados);
-        if (random.nextDouble() < probabilidadInfeccion) {
-            celula.setEstadoFuturo(Celula.ESTADO_CELULA_INFECTADA_A);
-        }
-    } else if (estado == Celula.ESTADO_CELULA_INFECTADA_A || estado == Celula.ESTADO_CELULA_INFECTADA_B) {
-        if (Math.random() < prepl) { // Usando prepl como probabilidad de recuperación
-            celula.setEstadoFuturo(Celula.ESTADO_CELULA_INFECTADA_A);
-        } else {
-            celula.setEstadoFuturo(Celula.ESTADO_CELULA_MUERTA);
-        }
-    } else if (estado == Celula.ESTADO_CELULA_MUERTA) {
-        double randomValue = Math.random();
-        if (randomValue < prepl) { // Usando prepl como probabilidad de resurrección
-            celula.setEstadoFuturo(Celula.ESTADO_CELULA_SANA);
-        } else if (randomValue >= prepl && randomValue < prepl + pinfec) { // Usando pinfec como probabilidad de infección
-            celula.setEstadoFuturo(Celula.ESTADO_CELULA_INFECTADA_A);
-        }
-    }
-}
+        case Celula.ESTADO_CELULA_MUERTA:
+            // Generar un número aleatorio en el rango [0, 1]
+            double random_prob = Math.random();
 
-
-private int contarVecinosInfectados(int i, int j) {
-    int vecinosInfectados = 0;
-    for (int x = -1; x <= 1; x++) {
-        for (int y = -1; y <= 1; y++) {
-            if (x == 0 && y == 0) continue;
-            int vecinoI = (i + x + L) % L;
-            int vecinoJ = (j + y + L) % L;
-            //Aun une las celulas, checar eso
-            if (matrizCelulas[vecinoI][vecinoJ].getEstado() != Celula.ESTADO_CELULA_SANA) {
-                vecinosInfectados++;
+            // Comparar el número aleatorio con las probabilidades
+            if (random_prob <= this.pinfec) {
+                celula_a_actualizar.setEstadoFuturo(Celula.ESTADO_CELULA_INFECTADA_A); // Reemplazar por una célula infectada tipo A
+                celula_a_actualizar.resetPeriodos(this.t);
+            } else if (random_prob <= (this.pinfec + this.prepl)) {
+                celula_a_actualizar.setEstadoFuturo(Celula.ESTADO_CELULA_SANA); // Reemplazar por una célula sana
             }
-        }
-    }
-    return vecinosInfectados;
-}
+            break;
 
-private void calcularDensidades() {
-    int celulasSanas = 0;
-    celulasInfectadasA = 0;
-    celulasInfectadasB = 0;
-    int celulasMuertas = 0;
-
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < L; j++) {
-            int estado = matrizCelulas[i][j].getEstado();
-            if (estado == Celula.ESTADO_CELULA_SANA) {
-                celulasSanas++;
-            } else if (estado == Celula.ESTADO_CELULA_INFECTADA_A) {
-                celulasInfectadasA++;
-            } else if (estado == Celula.ESTADO_CELULA_INFECTADA_B) {
-                celulasInfectadasB++;
-            } else if (estado == Celula.ESTADO_CELULA_MUERTA) {
-                celulasMuertas++;
-            }
+              default:
+         System.out.println("Si llegamos aquí, hubo un error en las estados de una célula");
+        break;
         }
     }
 
-    double densidadSanas = (double) celulasSanas / (L * L);
-    double densidadInfectadasA = (double) celulasInfectadasA / (L * L);
-    double densidadInfectadasB = (double) celulasInfectadasB / (L * L);
-    double densidadMuertas = (double) celulasMuertas / (L * L);
-
-    NodoDensidad nuevoNodo = new NodoDensidad(densidadSanas, densidadInfectadasA + densidadInfectadasB, densidadMuertas);
-    densidadActual.setSiguiente(nuevoNodo);
-    densidadActual = nuevoNodo;
-}
-
-public void imprimirDensidades() {
+    public void imprimirDensidades() {
     int tiempo = 0;
     NodoDensidad nodo;
     nodo = cabeceraDensidades;
@@ -170,23 +189,21 @@ public void imprimirDensidades() {
     tiempo++;
     }
 }
-    public void imprimirMatriz() {
-        for (int i = 0; i < L; i++) {
-            for (int j = 0; j < L; j++) {
-                System.out.print(matrizCelulas[i][j].getEstado() + " ");
-            }
-            System.out.println();
-        }
+
+
+    public Celula[][] getMatrizCelular() {
+    return this.matrizCelulas;
+    }
+    public NodoDensidad getDensidadActual() {
+    return this.densidadActual;
     }
     public int getL(){
         return this.L;
     }
-
-    public Celula[][] getMatrizCelulas() {
-        return this.matrizCelulas;
+    
+    public int getT(){
+           return this.t;
     }
     
-    
 }
-
 
